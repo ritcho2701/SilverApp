9f0b2f8cb6511ec56d4c47713f240b520e83e7eb
ritcho2701 <rrpatil2701@gmail.com>
2023-12-14 16:29:05 +0530

    connect azure database

--- patient.py
+++ patient.py
@@ -47,10 +47,10 @@ def establish_mysql_connection():
     try:
         # Establish MySQL connection
         conn = mysql.connector.connect(
-            host='127.0.0.1',
-            user='root',
-            password='Ritcho@2701',
-            database='SilverLine_database'
+            host='silverapp.mysql.database.azure.com',
+            user='rushi2701',
+            password='User@2701',
+            database='silverapp'
         )
         return conn
     except mysql.connector.Error as err:
